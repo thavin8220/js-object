fcff83077136a06d4b3d040e34e9f8b0dcd9e286
thavin8220 <thavinmurugan@gmail.com>
2022-12-21 11:41:47 +0530

    add file

---
+++ main.js
@@ -0,0 +1,41 @@
+let friuts={
+    apple:"redcolor",
+    banana:"yellow",
+    mango:"yellow",
+    orange:"orangecolor",
+    grapes:"black"
+}
+console.log(friuts);
+
+let car={
+    name:"maruti suzuki swift",
+    color:"red",
+    rate:"5.91lakh",
+    millage:"22.5kmpl",
+}
+console.log(car);
+
+let bike={
+    name:"passionpro",
+    color:"black",
+    model:"2014",
+    millage:"55kmpl",
+    condition:"good",
+}
+console.log(bike);
+
+let person={
+    name:"Thavin",
+    height:"5.5",
+    weight:"58kg",
+    age:"22",
+}
+console.log(person);
+
+let laptop={
+    name:"lenova",
+    model:"2016",
+    color:"black",
+    software:"AMD PRO",
+}
+console.log(laptop)
